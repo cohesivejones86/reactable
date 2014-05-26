109054fc069ebbfc0cb01627d17d0a2dcdbadadb
Nate Jones <lemaitre86@gmail.com>
2014-05-26 13:56:07 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reactable::Application.config.secret_token = 'e28781cca49c25cc0dca7a3d22674288c6257bfd72b14b1501998a2c8dcf770c4fac5833d530182cc4983663d88f539433428a0258d4a3a494ac715dedc98d0d'
